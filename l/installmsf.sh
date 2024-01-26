fd96e68a8f2f212792d1ca44c2d7a3b1be40c49e
bugflixer <134541455+bugflixer@users.noreply.github.com>
2024-01-27 01:19:09 +0530

    Update installmsf.sh

--- l/installmsf.sh
+++ l/installmsf.sh
@@ -1,200 +1,86 @@
 #!/data/data/com.termux/files/usr/bin/bash
-sleep 2
-clear
-echo "
-    +-+-+-+-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+
-    |M|e|t|a|s|p|l|o|i|t| |i|n| |T|e|r|m|u|x|
-    +-+-+-+-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+
-            +-+-+ +-+-+-+-+-+-+-+-+-+-+
-             |b|y| |B|u|g|f|l|i|x|e|r|
-            +-+-+ +-+-+-+-+-+-+-+-+-+-+
-"center() {
+
+# Banner and center functions
+center_banner() {
+    local termwidth=$(stty size | cut -d" " -f2)
+
+    local banner=(
+        "+-+-+-+-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+"
+        "|M|e|t|a|s|p|l|o|i|t| |i|n| |T|e|r|m|u|x|"
+        "+-+-+-+-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+"
+        "+-+-+ +-+-+-+-+-+-+-+-+-+-+"
+        "|b|y| |B|u|g|f|l|i|x|e|r|"
+        "+-+-+ +-+-+-+-+-+-+-+-+-+-+"
+    )
+
+    echo -e "\e[34m" # Blue color
+    for line in "${banner[@]}"; do
+        printf "%*s\n" $(( (termwidth + ${#line}) / 2 )) "$line"
+    done
+    echo -e "\e[0m" # Reset color
+}
+
+center() {
   termwidth=$(stty size | cut -d" " -f2)
   padding="$(printf '%0.1s' ={1..500})"
   printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
 }
-pkg install pv -y >/dev/null 2>&1
-sleep 3
-echo "𝙺𝚒𝚗𝚍𝚕𝚢 𝚘𝚙𝚎𝚗 𝚢𝚘𝚞𝚛 𝚒𝚗𝚝𝚎𝚛𝚗𝚎𝚃" | pv -qL 45
-sleel 2
-if [ -z "$USER_ACCESS_GRANTED" ]; then
+
 clear
-echo -e "${RED} BOSDK ${GREEN}KEY DAAL PEHLE 😂😂😂 ...."
-echo "𝙸𝚝 𝚠𝚒𝚕𝚕 𝚝𝚊𝚔𝚎 𝚜𝚘𝚖𝚎 20-30 𝚖𝚒𝚗𝚜 𝚍𝚎𝚙𝚎𝚗𝚍𝚒𝚗𝚐 𝚘𝚗 𝚢𝚘𝚞𝚛 𝚒𝚗𝚝𝚎𝚛𝚗𝚎𝚃" | pv -qL 45
-sleep 2
-echo "𝙺𝚒𝚗𝚍𝚕𝚢 𝚋𝚎 𝚘𝚗𝚕𝚒𝚗𝚎 𝚊𝚗𝚍 𝚋𝚎 𝚙𝚊𝚝𝚒𝚎𝚗𝚝𝚃" | pv -qL 45
-sleep 3
-clear
-print_pgp_key() {
-  cat <<-EOF
------BEGIN PGP PUBLIC KEY BLOCK-----
+center_banner
 
-mQINBFDAy/0BEAC8I5bw5gLQqHKx5JCacYcXFL6AZowl3qIOTxo5yfBl8CepNpWY
-OOERvIUJb17WehhhbWOo9WjpBalDXBRtI1NvfArewOT8fLm7BdhYe8U45moBfkYi
-xFtNrPw3pdIltHQISrB8PufhliN8obQuq0rcxYV8NblvYo4gIGNjBfO1QGvBNmp7
-kBtjlAuZguScZmUTdPOwfv8fqN52X9tCv1ahQk1hg8XG9YwW0vXb5z93jkLXBb5b
-sRCnou4m9IV6vOv2HVNRyMKT7uht3z4FqflP9NkySl4daCdZgmXbf169vvLdwLrC
-lVymwAbwvuyILZv4JW1w0Kx8nWiTuK5A886882i83lxnkh1vC9jInva4/5hTrbRw
-XJb7qOyh7sxa5GOfgq1NwVfLkrvVCMystrPu18sF1ORfg1UTFcz86RYdxpmoZvk7
-EeABiLCQDZKOf0fV3U9CxLj8gXPjPY1Lu6udZUN6NG1ALJjsPkGnbpQEqEJlKNAG
-+rF+tp73TrG0PW8C/THL7fN93ET3wn5tfNu86Liui9wd8ZLuPJNEYeE6eyPAgXJ4
-p69Yb4ou5um5jWnzaVameECBZvtc4HOhy3nTEiVMDcKv/o8XxKOCLpjW1RSDirKl
-ZRIsJYPx2yuJSVMCsN5Sghp5+OCsQ+On4OFWxCskemvy97ftkv/fwUI7mQARAQAB
-tCJNZXRhc3Bsb2l0IDxtZXRhc3Bsb2l0QHJhcGlkNy5jb20+iQJUBBMBCAA+AhsD
-BQsJCAcDBRUKCQgLBRYCAwEAAh4BAheAFiEECeVfr094Ys1tVYmXzftfpSAHuVQF
-Al1xL2oFCR98Zm0ACgkQzftfpSAHuVTPlg/9H++FCAMEoQxxWeQ1e7RkQbplrjmA
-+w1hqto1YnJDB3RFpvEubS45h/36Lgs1SmcgGx1dw2uzjSAtWS/4MWtvnyWXFV3K
-ZjhyJAlNw7bZLcrJHqpGFdVJvRuPmf6dYvPgSaqZQv0HP2fwSwu/msGJ8u1E7kDW
-KpTg5LeQlJ3F3eePSAIa47Y0H6AaNuiW1lUz4YTboRKfDRYQizfKKi/9ssqAXNI5
-eAPLhj9i3t/MVSGtV2G6xldEQLM7A0CI4twrIplyPlYt5tCxdA225cRclRYbqaQX
-AcE34YJWAWCgGxw98wxQZwtk8kXSwPdpMyrHadaAHiTzqPBlTrSes8sTDoJxfg8P
-k73ILgBIey4FD7US5V46MZrKtduFmL9OvqTvZl17r6xaoScrH4oK690VHmdkfM2P
-KOkgRU8PumlIjGvTDavm5afh6LkD75XDLPF5n9Om7F+Sc+2Ul+SPYV8kQaFHX1XD
-QuHBeJRT9VdO9T/SI2YHkCnatC50nr9V/gK2ecui+ri8gto29jaAmz7IhdNlMU9k
-EPfAbnG6Mu6DLlpjsTBYEyuAnmKVWvNBDlgC4d42WQMGleeSXCZzC0Wh3t9FbBOc
-3+OB1aEdUrx1dE0elWyrzUFHmd/EOCXpLSE4RYcN6TuCIkEI0TyXYmDRQWGofK0G
-S8CxmfmppfGI92C5Ag0EUMDL/QEQALkDKrnosJ5erN/ot2WiaM82KhI30J6+LZUL
-9sniuA1a16cfoQfwXTnFpcd48O41aT2BNp0jpGjDo49rRC8yB7HjCd1lM+wRRm/d
-0Et/4lBgycaa63jQtG+GK9gN+sf4LkiDgJYkXX2wEOilvZw9zU2VLTGhOUB+e7vR
-P2LpnA4nSkvUGNKvaWcF+k/jeyP2o7dorXumfXfjGBAYiWCF6hDiy8XT5G2ruMDD
-lWafoleGSVeuB0onijqzRU5BaN+IbMIzGWLRP6yvhYmmO1210IGZBF3/gJLR3OaU
-m82AV5Eg4FslzBViv620hDuVsEoeRne2uN/qiEtYjSLJWYn5trtApQkk/1i+OK6c
-/lqtT+CyQ/IS69E5+fJYkAYkCgHJBdcJmDXSHKycarDDihPSPuN131kgyt/wZLE9
-oV6eeH5ay9ruto9NYELNjmGVrZyZyAYRo6duN/ZyUBbczIaaWVCkEYgO04rwamkT
-wOdWGEzj24gNMcXYCKQyW2OrDN3odX3f1UDvsiZqX88o0fI5YQB2YhGBjAfH5wSP
-MkBBJCR3Qbc9J8ksFp//RWjWcFq/yr1WOCqEQVo1PMSPkeqfqV3ApS6XhVv4ChKL
-PlnV27fa6XUK1yjNQlNxYkv15tnxhtKrLs6XiyVJbe6Q1obq0FOpBhv2WIh291BQ
-bqgmGbNvABEBAAGJAjwEGAEIACYCGwwWIQQJ5V+vT3hizW1ViZfN+1+lIAe5VAUC
-XXEvjgUJH3xmkQAKCRDN+1+lIAe5VJueD/4+6ldtpXYin+lWcMyHM8487GczLi8S
-XgxZJu/2GzEpgdke8xoQWv6Jsk2AQaPLciIT7yU7/gTWsOiY7Om+4MGqZY+KqZ/X
-eI8nFsGQx2yI7TDUQasN4uB5y6RnMGSH8DbAIWydVP2XWNVCHcVNMbeAoW7IiOOh
-I2wT4bCmzrjfVsJRo8VvpykPhm7+svsU2ukMW0Ua77bA1gzdvPpRzN2I1MY/6lJk
-x7BwtYsiAZt0+jII31IdCNpz4BlU3eadG+QbEH/q5FrHPBtkRWmziJpKXZDWdAg/
-I7yim36xfxjMtcv8CI3YKmy5jYcGKguA2SGApQpPEUkafLZc62v8HVmZZFKmLyXR
-XM9YTHz4v4jhruJ80M6YjUtfQv0zDn2HoyZuPxAW4HCys1/9+iAhuFqdt1PnHBs/
-AmTFlQPAeMu++na4uc7vmnDwlY7RDPb0uctUczhEO4gT5UkLk5C9hcOKVAfmgF4n
-MNgnOoSZO2orPKh3mejj+VAZsr1kfEWMoFeHPrWdxgRmjOhUfy6hKhJ1H306aaSQ
-gkE3638Je/onWmnmZrDEZq7zg0Qk3aOOhJXugmRnIjH341y/whxvAdJIyXrjLN4z
-qCU0JkA1rVqS6PXZabKb9DOqYa4pr9thGS5rU+Gn3GWiSq2PtVW6Hh83WOFcEsMk
-2vTa24LE0J2DQg==
-=Qa/n
------END PGP PUBLIC KEY BLOCK-----
-EOF
-}
+# Loading spinner
+center "Loading..."
+source <(echo "c3Bpbm5lcj0oICd8JyAnLycgJy0nICdcJyApOwoKY291bnQoKXsKICBzcGluICYKICBwaWQ9JCEKICBmb3IgaSBpbiBgc2VxIDEgMTBgCiAgZG8KICAgIHNsZWVwIDE7CiAgZG9uZQoKICBraWxsICRwaWQgIAp9CgpzcGluKCl7CiAgd2hpbGUgWyAxIF0KICBkbyAKICAgIGZvciBpIGluICR7c3Bpbm5lcltAXX07IAogICAgZG8gCiAgICAgIGVjaG8gLW5lICJcciRpIjsKICAgICAgc2xlZXAgMC4yOwogICAgZG9uZTsKICBkb25lCn0KCmNvdW50" | base64 -d)
 
-install_deb() {
-  LIST_FILE=/etc/apt/sources.list.d/metasploit-framework.list
-  PREF_FILE=/etc/apt/preferences.d/pin-metasploit.pref
-  echo -n "Adding metasploit-framework to your repository list.."
-  echo "deb $DOWNLOAD_URI/apt lucid main" > $LIST_FILE
-  print_pgp_key | apt-key add -
-  if [ ! -f $PREF_FILE ]; then
-    mkdir -p /etc/apt/preferences.d/
-    cat > $PREF_FILE <<EOF
-Package: metasploit*
-Pin: origin downloads.metasploit.com
-Pin-Priority: 1000
-EOF
-  fi
-  echo -n "Updating package cache.."
-  apt-get update > /dev/null
-  echo "OK"
-  echo "Checking for and installing update.."
-  apt-get install -y --allow-downgrades metasploit-framework
-}
+# Dependencies Installation
+center "* Dependencies installation..."
+pkg update -y
+pkg upgrade -y -o Dpkg::Options::="--force-confnew"
+pkg install -y binutils python autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner pkg-config git ruby -o Dpkg::Options::="--force-confnew"
+python3 -m pip install requests
 
-install_rpm() {
-  echo "Checking for and installing update.."
-  REPO_FILE=/etc/yum.repos.d/metasploit-framework.repo
-  GPG_KEY_FILE=/etc/pki/rpm-gpg/RPM-GPG-KEY-Metasploit
-  echo -n "Adding metasploit-framework to your repository list.."
+# Fix ruby BigDecimal 
+#center "* Fix ruby BigDecimal"
+#source <(curl -sL https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt)
 
-  cat > /etc/yum.repos.d/metasploit-framework.repo <<EOF
-[metasploit]
-name=Metasploit
-baseurl=$DOWNLOAD_URI/rpm
-gpgcheck=1
-gpgkey=file://$GPG_KEY_FILE
-enabled=1
-EOF
-  print_pgp_key > ${GPG_KEY_FILE}
-  yum install -y metasploit-framework
-}
-
-install_suse() {
-  echo "Checking for and installing update.."
-  GPG_KEY_FILE_DIR=/etc/pki/rpm-gpg
-  GPG_KEY_FILE=${GPG_KEY_FILE_DIR}/RPM-GPG-KEY-Metasploit
-  echo -n "Adding metasploit-framework to your repository list.."
-  if [ ! -d $GPG_KEY_FILE_DIR ]; then
-    mkdir -p $GPG_KEY_FILE_DIR
-  fi
-  zypper ar  -f $DOWNLOAD_URI/rpm metasploit
-  print_pgp_key > ${GPG_KEY_FILE}
-  rpmkeys --import ${GPG_KEY_FILE}
-  zypper install -y metasploit-framework
-}
-
-install_pkg()
-{
-  (
-    cd ~/Downloads
-
-    echo "Downloading package..."
-    curl -O "$DOWNLOAD_URI/osx/metasploitframework-latest.pkg"
-
-    echo "Checking signature..."
-
-    if pkgutil --check-signature metasploitframework-latest.pkg; then
-      echo "Installing package..."
-      installer -pkg metasploitframework-latest.pkg -target /
-    fi
-
-    echo "Cleaning up..."
-    rm -fv metasploitframework-latest.pkg
-  )
-}
-
-DOWNLOAD_URI=http://downloads.metasploit.com/data/releases/metasploit-framework
-PKGTYPE=unknown
-ID=`id -u`
-
-if [ -f /etc/redhat-release ] ; then
-  PKGTYPE=rpm
-elif [ -f /etc/system-release ] ; then
-  # If /etc/system-release is present, this is likely a distro that uses RPM.
-  PKGTYPE=rpm
-else
-  if uname -sv | grep 'Darwin' > /dev/null; then
-    PKGTYPE=pkg
-  elif [ -f /usr/bin/zypper ] ; then
-    PKGTYPE=sus
-  else
-    PKGTYPE=deb
-  fi
+# Erase Old Metasploit Folder
+center "* Erasing old metasploit folder..."
+if [ -d "${PREFIX}/opt/metasploit-framework" ]; then
+  rm -rf ${PREFIX}/opt/metasploit-framework
 fi
 
-if [ "$ID" -ne 0 ]; then
-  if ! hash sudo 2>/dev/null; then
-    echo "This script must be executed as the 'root' user or with sudo"
-    exit 1
-  else
-    echo "Switching to root user to update the package"
-    sudo -E $0 $@
-    exit 0
-  fi
+# Download Metasploit
+center "* Downloading..."
+if [ ! -d "${PREFIX}/opt" ]; then
+  mkdir ${PREFIX}/opt
 fi
+git clone https://github.com/rapid7/metasploit-framework.git --depth=1 ${PREFIX}/opt/metasploit-framework
 
-case $PKGTYPE in
-  deb)
-    install_deb
-    ;;
-  sus)
-    install_suse
-    ;;
-  rpm)
-    install_rpm
-    ;;
-  *)
-    install_pkg
-esac
+# Install Metasploit
+center "* Installation..."
+cd ${PREFIX}/opt/metasploit-framework
+gem install bundler
+NOKOGIRI_VERSION=$(cat Gemfile.lock | grep -i nokogiri | sed 's/nokogiri [\(\)]/(/g' | cut -d ' ' -f 5 | grep -oP "(.).[[:digit:]][\w+]?[.].")
+# by overriding cflags nokogiri will install or you can simply declare a void function 
+#  you might have seen this error while installing nokogiri `xmlSetStructuredErrorFunc((void *)rb_error_list, Nokogiri_error_array_pusher);`
+#  solution : void xmlSetStructuredErrorFunc(void *rb_error_list, void *Nokogiri_error_array_pusher); you can set any parameter name 
+#  for sake of simplicity tweaking cflags is better than declaring a void function for every c file
+
+gem install nokogiri -v $NOKOGIRI_VERSION -- --with-cflags="-Wno-implicit-function-declaration -Wno-deprecated-declarations -Wno-incompatible-function-pointer-types" --use-system-libraries
+bundle install
+gem install actionpack
+bundle update activesupport
+bundle update --bundler
+bundle install -j$(nproc --all)
+
+# Link Metasploit Executables
+ln -sf ${PREFIX}/opt/metasploit-framework/msfconsole ${PREFIX}/bin/
+ln -sf ${PREFIX}/opt/metasploit-framework/msfvenom ${PREFIX}/bin/
+ln -sf ${PREFIX}/opt/metasploit-framework/msfrpcd ${PREFIX}/bin/
+termux-elf-cleaner ${PREFIX}/lib/ruby/gems/*/gems/pg-*/lib/pg_ext.so
+
+echo -e "\033[32m" # Blue color
+center "Installation complete"
+echo -e "\nStart Metasploit using the command: msfconsole"
+echo -e "\033[0m" # Reset color
